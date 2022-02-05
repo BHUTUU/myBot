fcea449b239232212a1897102274c249e1e61266
Suman Kumar ~BHUTUU <raj259942@gmail.com>
2022-02-05 15:58:21 +0530

    base completed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from telegram.ext.callbackcontext import CallbackContext
 from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
-import requests
+import requests, os
 #<<<======Variable Declaration========>>>#
 TOKEN="1963665302:AAFKNH7N7LBZpdQAsZKi-RVXgDqB1w1oosU"
 updater = Updater(TOKEN, use_context=True)
@@ -13,44 +13,50 @@ helpText="""
 =============================
 command             usage
 ----------------------------------------------------------
-/help     |- for this help menu.
-/say      |- only for BHUTUU 👻.
-/fetch   |- To fetch sysinfo.
-/battery|- To fetch battery.
+/help            |- for this help menu.
+/github        |- BHUTUU's github 👻.
+/website     |- BHUTUU's website.
+/youtube     |- BHUTUU's youtube
+/webclock  |- To open WebClock
+/calculator |- To open calculator
+/star            |- To see stars ;)
 ______________________________________
 """
 
 #<<<======Function Declaration=========>>>#
-def define(update: Update, context: CallbackContext):
-    word=update.message.text
-    response = requests.get(f"http://api.urbandictionary.com/v0/define?term={word}")
-    parseJson=json.loads(response)
-    update.message.reply_text(parseJson["definiton"])
-def start(update: Update, context: CallbackContext): 
-    update.message.reply_text("""Hello sir, Welcome to the Bot.Please write /help to see the commands available.""") 
-def help(update: Update, context: CallbackContext): 
-    update.message.reply_text(helpText) 
-def gmail_url(update: Update, context: CallbackContext): 
-    update.message.reply_text("""Your gmail link here (I am not\ 
-        giving mine one for security reasons)""")
-def youtube_url(update: Update, context: CallbackContext): 
-    update.message.reply_text("""Youtube Link => https://www.youtube.com/""") 
-def linkedIn_url(update: Update, context: CallbackContext): 
-    update.message.reply_text("""LinkedIn URL => https://www.linkedin.com/in/dwaipyan-bandyopadhyay-007a/""")
-def geeks_url(update: Update, context: CallbackContext): 
-    update.message.reply_text("""GeeksforGeeks URL => https://www.geeksforgeeks.org/""") 
-def unknown(update: Update, context: CallbackContext): 
-    update.message.reply_text("""Sorry '%s' is not a valid command""" % update.message.text) 
-def unknown_text(update: Update, context: CallbackContext): 
-    update.message.reply_text("""Sorry I can't recognize you , you said '%s'""" % update.message.text)
+#def expl(update: Update, context: CallbackContext):
+#    word=update.message.text.split(' ')[1]
+#    response = requests.get(f"http://api.urbandictionary.com/v0/define?term={word}")
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("""Hello sir, Welcome to the Bot.Please write /help to see the commands available.""")
+def help(update: Update, context: CallbackContext):
+    update.message.reply_text(helpText)
+def github_url(update: Update, context: CallbackContext):
+    update.message.reply_text("""https://github.com/BHUTUU""")
+def youtube_url(update: Update, context: CallbackContext):
+    update.message.reply_text("""https://www.youtube.com/channel/UCcbIIYUlcfjkYLHQ5EklztA""")
+def website_url(update: Update, context: CallbackContext):
+    update.message.reply_text("""bhutuu.github.io""")
+def calculator_url(update: Update, context: CallbackContext):
+    update.message.reply_text("""bhutuu.github.io/calculator""")
+def shootingStar_url(update: Update, context: CallbackContext):
+    update.message.reply_text("""bhutuu.github.io/shootingStar""")
+def clockGui_url(update: Update, context: CallbackContext):
+    update.message.reply_text("""bhutuu.github.io/clockgui""")
+#def unknown(update: Update, context: CallbackContext):
+#    update.message.reply_text("""Sorry '%s' is not a valid command""" % update.message.text)
+#def unknown_text(update: Update, context: CallbackContext):
+#    update.message.reply_text("""Sorry I can't recognize you , you said '%s'""" % update.message.text)
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('youtube', youtube_url)) 
-updater.dispatcher.add_handler(CommandHandler('help', help)) 
-updater.dispatcher.add_handler(CommandHandler('linkedin', linkedIn_url)) 
-updater.dispatcher.add_handler(CommandHandler('gmail', gmail_url)) 
-updater.dispatcher.add_handler(CommandHandler('geeks', geeks_url)) 
-updater.dispatcher.add_handler(CommandHandler('define', define))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown)) 
-updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
-updater.start_polling() 
+updater.dispatcher.add_handler(CommandHandler('youtube', youtube_url))
+updater.dispatcher.add_handler(CommandHandler('help', help))
+updater.dispatcher.add_handler(CommandHandler('website', website_url))
+updater.dispatcher.add_handler(CommandHandler('github', github_url))
+updater.dispatcher.add_handler(CommandHandler('star', shootingStar_url))
+updater.dispatcher.add_handler(CommandHandler('webclock', clockGui_url))
+updater.dispatcher.add_handler(CommandHandler('calculator', calculator_url))
+#updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
+#updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+#updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
+updater.start_polling()
