552a7259147d04349384cf0e5dabe92f17de92b4
Suman Kumar ~BHUTUU <raj259942@gmail.com>
2022-02-05 14:32:40 +0530

    changed whole thing

--- urban.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-read -p "Enter your word: " WORD
-RESPONSE=$(curl -fsSL "http://api.urbandictionary.com/v0/define?term=${WORD}" | jq -r .list[].definition)
-printf "RESULTS:+\n${RESPONSE}\n"
